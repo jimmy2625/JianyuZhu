fdcc12f46579889642369569e54106466d9f4918
jimmy2625 <jimmy26251@gmail.com>
2023-12-09 16:33:37 +0100

    med overflow

--- scripts.js
+++ scripts.js
@@ -9,3 +9,33 @@ window.onscroll = function() {
         header.classList.remove('navbarDark');
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    var navLinks = document.querySelectorAll('.nav-link');
+    var navbarCollapse = document.querySelector('.navbar-collapse');
+    navLinks.forEach(function (navLink) {
+        navLink.addEventListener('click', function () {
+            navbarCollapse.classList.toggle('show');
+        });
+    });
+});
+
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+    // Get all service cards
+    const serviceCards = document.querySelectorAll('.service-card');
+
+    // Find the maximum height among all service cards
+    let maxHeight = 0;
+    serviceCards.forEach(card => {
+        const cardHeight = card.offsetHeight;
+        if (cardHeight > maxHeight) {
+            maxHeight = cardHeight;
+        }
+    });
+
+    // Set the maximum height to all service cards
+    serviceCards.forEach(card => {
+        card.style.height = `${maxHeight}px`;
+    });
+});
