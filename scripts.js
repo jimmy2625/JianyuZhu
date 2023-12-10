be40af7a03752049175d500568ddb3dfb6d84bdc
jimmy2625 <jimmy26251@gmail.com>
2023-12-10 14:30:30 +0100

    final version med cookies

--- scripts.js
+++ scripts.js
@@ -19,3 +19,24 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var navLinks = document.querySelectorAll('.navbar-nav a');
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+
+            event.preventDefault();
+
+            var targetId = this.getAttribute('href').substring(1);
+            var targetSection = document.getElementById(targetId);
+            var offsetTop = targetSection.offsetTop;
+
+            var newOffsetTop = offsetTop - 100;
+
+            window.scrollTo({
+                top: newOffsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
